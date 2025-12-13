5dfe85cf85a102a124750c0e2accba1d79d37566
Mobinaalivandi <mobinavafa2006@gmail.com>
2025-12-13 01:27:37 -0800

    Create list3.java

---
+++ supermarket /list3.java
@@ -0,0 +1,18 @@
+public class list3 {
+    private double price;
+    public String name;
+    private int quantity;
+
+    list3(double price, int quantity) {
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double getprice() {
+        return price;
+    }
+
+    public int getquantity() {
+        return quantity;
+    }
+}

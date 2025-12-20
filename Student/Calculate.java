d5684e28aefbd1c627bce9ec55477f5933bca6b6
Mobinaalivandi <mobinavafa2006@gmail.com>
2025-12-20 09:43:18 -0800

    Update Calculate.java

--- Student/Calculate.java
+++ Student/Calculate.java
@@ -2,11 +2,10 @@ public class Calculate {
     public void check (Student[] list) {
         for ( int i = 0 ; i < list.length ; ++i) {
             if (list[i].getgrade() > 60 ) {
-                System.out.println(list[i].getname() + " " + "has passed");
+                System.out.println(list[i].getname() + " " + "has passed" + " " + "with the grade : " + " " + list[i].getgrade());
             } else {
-                System.out.println(list[i].getname() + " " + "has failed");
+                System.out.println(list[i].getname() + " " + "has failed" +  " " + "with the grade : " + " " + list[i].getgrade());
             }
         }
     }
 }
-

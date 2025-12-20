f6587204e9cb0a922c89154d6e846d7e787fa399
Mobinaalivandi <mobinavafa2006@gmail.com>
2025-12-20 09:25:44 -0800

    Add input handling and student processing logic

--- Student/Main.java
+++ Student/Main.java
@@ -1,1 +1,17 @@
-
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        Calculate cal = new Calculate();
+        Student[] list = new Student[n];
+        for (int i = 0; i < n; ++i) {
+            input.nextLine();
+            String name = input.nextLine();
+            int grade = input.nextInt();
+            list[i] = new Student(name, grade);
+        }
+        list[0].printinfo(list);
+        cal.check(list);
+    }
+}

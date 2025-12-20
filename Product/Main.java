740c9457c8cd62d5107e04a295ff0ecaeef8e73e
Mobinaalivandi <mobinavafa2006@gmail.com>
2025-12-20 09:33:27 -0800

    Update Main.java

--- Product/Main.java
+++ Product/Main.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Calculate cal = new Calculate();
+        Product[] list = new Product[5];
+        for (int i = 0; i < 5; ++i) {
+            String name = input.nextLine();
+            double price = input.nextDouble();
+            input.nextLine();
+            list[i] = new Product(name, price);
+        }
+        list[0].printinfo(list);
+        cal.check(list);
+    }
+}
 

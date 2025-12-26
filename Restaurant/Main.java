0a542ec9247b0b89bcd5a519b31d3febaf1a303b
Mobinaalivandi <mobinavafa2006@gmail.com>
2025-12-26 04:36:53 -0800

    Update Main.java

--- Restaurant/Main.java
+++ Restaurant/Main.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        input.nextLine();
+        System.out.println("Number of Orders : " + " " + n);
+        Order[] foods = new Order[n];
+        for (int i = 0; i < n; ++i) {
+            String food = input.nextLine();
+            int quantity = input.nextInt();
+            double price = input.nextDouble();
+            input.nextLine();
+            foods[i] = new Order(food, quantity, price);
+        }
+        foods[0].biggestquantity(foods);
+        foods[0].receipt(foods);
+    }
+}
 

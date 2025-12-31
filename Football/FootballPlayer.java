80877ef72b7f244bcba92eaa5d3e55ce2a23121b
Mobinaalivandi <mobinavafa2006@gmail.com>
2025-12-31 15:03:35 +0330

    Add files via upload

---
+++ Football/FootballPlayer.java
@@ -0,0 +1,27 @@
+package Model;
+
+public class FootballPlayer {
+    private String name;
+    private String position;
+    private int age;
+    private int goals;
+
+    public FootballPlayer(String name, String position, int age, int goals) {
+        this.name = name;
+        this.position = position;
+        this.age = age;
+        this.goals = goals;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public String getposition() { return position; }
+
+    public int getage() {
+        return age;
+    }
+
+    public int getgoals() { return goals; }
+}

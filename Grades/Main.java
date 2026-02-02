f7544f20b10a695beae8709dcc2cea20ad589278
Mobinaalivandi <mobinavafa2006@gmail.com>
2026-02-02 05:29:33 -0800

    Update Main.java

--- Grades/Main.java
+++ Grades/Main.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        input.nextLine();
+        Students list[] = new Students[n];
+        for (int i = 0; i < n; i++) {
+            String name = input.nextLine();
+            int grade = input.nextInt();
+            if ( grade < 0 || grade > 20) {
+                System.out.println("This grade is not acceptable");
+                i--;
+                continue;
+            }
+            input.nextLine();
+            list[i] = new Students(name, grade);
+        }
+        list[0].average(list);
+        list[0].highestgrade(list);
+        list[0].passed(list);
+    }
+}
 

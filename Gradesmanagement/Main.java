a1d78affb87930593298def27d3f6c2a1fd893a6
Mobinaalivandi <mobinavafa2006@gmail.com>
2026-02-09 10:27:30 -0800

    Update Main.java

--- Gradesmanagement/Main.java
+++ Gradesmanagement/Main.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        while () {
-            int n = input.nextInt();
+        int n = 0;
+        while (true) {
+            n = input.nextInt();
             if (n < 0) {
                 System.out.println("This amount is not acceptable , try again");
                 continue;
+            } else {
+                break;
             }
+        }
             Student[] list = new Student[n];
             for (int i = 0; i < list.length; i++) {
                 double grades = input.nextDouble();
@@ -18,32 +22,31 @@ public class Main {
                 }
                 list[i] = new Student(grades);
             }
-
-            while (true) {
-                int choice = input.nextInt();
-                if (choice > 5) {
-                    System.out.println("Not acceptable . Enter another number");
-                    continue;
-                }
-                System.out.println("Choose an option from the menu below");
-                System.out.println("1.Calculate the avarge ");
-                System.out.println("2.Find maximum and minimum score");
-                System.out.println("3.Count passed and failed student");
-                System.out.println("4.Show grades above average");
-                System.out.println("5.Exit");
-
-                if (choice == 1) {
-                    list[0].average(list);
-                } else if (choice == 2) {
-                    list[0].maximin(list);
-                } else if (choice == 3) {
-                    list[0].passedfailed(list);
-                } else if (choice == 4) {
-                    list[0].aboveaverage(list);
-                } else {
-                    break;
-                }
+        while (true) {
+            System.out.println("Choose an option from the menu below");
+            System.out.println("1.Calculate the avarge ");
+            System.out.println("2.Find maximum and minimum score");
+            System.out.println("3.Count passed and failed student");
+            System.out.println("4.Show grades above average");
+            System.out.println("5.Exit");
+            int choice = input.nextInt();
+            if (choice > 5 || choice < 1) {
+                System.out.println("Not acceptable . Enter another number");
+                continue;
+            }
+            if (choice == 1) {
+                list[0].average(list);
+            } else if (choice == 2) {
+                list[0].maximin(list);
+            } else if (choice == 3) {
+                list[0].passedfailed(list);
+            } else if (choice == 4) {
+                list[0].aboveaverage(list);
+            } else {
+                break;
             }
         }
     }
 }
+
+
